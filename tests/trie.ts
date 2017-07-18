0f97931048fa32e220f97c732f2ff3cdf6ce7c0c
Bozidar Dautovic <bozidardautovic@gmail.com>
2017-07-19 00:18:57 +0200

    Add method tests

--- tests/trie.ts
+++ tests/trie.ts
@@ -1,10 +1,27 @@
+import * as assert from "assert";
 import { Trie } from "../src/trie";
 
-const trie = new Trie();
+describe("Trie", () => {
+    const trie: Trie = new Trie();
 
-trie.insert("Bozidar");
-trie.insert("Dautovic");
-trie.insert("Boki");
+    describe("#insert()", () => {
+        it("should insert three strings 'Bozidar', 'Tree' and 'Trie'", () => {
+            trie.insert("Bozidar");
+            trie.insert("Tree");
+            trie.insert("Trie");
+            assert.equal(true, trie.search("Bozidar"));
+        });
+    });
 
-console.warn(trie.search("Bozidar"));
-console.warn(trie.searchNode("Bozi"));
+    describe("#search()", () => {
+        it("should find 'Tree' string", () => {
+            assert.equal(true, trie.search("Tree"));
+        });
+    });
+
+    describe("#startsWith()", () => {
+        it("should find string with 'Tr' prefix", () => {
+            assert.equal(true, trie.startsWith("Tr"));
+        });
+    });
+});
